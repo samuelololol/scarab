d866144a6300ea06421952a3064aee9893021399
samuelololol <samuelololol@gmail.com>
2015-11-26 14:03:55 +0800

    Fix ConfigParser version compatiblity

--- scarab/test/common/utils.py
+++ scarab/test/common/utils.py
@@ -8,6 +8,7 @@ import codecs
 import os
 import ConfigParser
 import io
+import sys
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -20,7 +21,10 @@ def db_config(key):
     config_path = os.path.join(config_path, 'development.ini') #scarab/development.ini
     with open(config_path, 'r') as f:
         config_content = f.read()
-    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    if sys.version_info[0:2] == (2,6):
+        config = ConfigParser.RawConfigParser()
+    else:
+        config = ConfigParser.RawConfigParser(allow_no_value=True)
     config.readfp(io.BytesIO(config_content))
     DB_URL = config.get('app:main', key)
     return DB_URL
